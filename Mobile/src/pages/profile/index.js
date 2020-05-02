6f8c88844e649bfc3269186c5895b555a77d01e5
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2020-05-01 23:57:22 -0300

    Implementação visual inicial dos itens da "WishList" no "myProfile"

--- Mobile/src/pages/profile/index.js
+++ Mobile/src/pages/profile/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
-import { FontAwesome5, MaterialIcons } from '@expo/vector-icons';
+import { View, Image, Text, TouchableOpacity, Modal } from 'react-native';
+import {
+    FontAwesome5,
+    MaterialIcons,
+    FontAwesome,
+    Feather,
+} from '@expo/vector-icons';
 
 import { style } from './styles';
 import UserImage from '../../assets/profile/userImage.png';
+import CategoryIcon from '../../assets/main/categoryIcon.png';
+import ProductImage from '../../assets/main/productImage.png';
 
 export default function Profile() {
     return (
@@ -75,6 +82,70 @@ export default function Profile() {
                     Quer me enviar um presente e não sabe o que? Essa é uma
                     lista de alguns que eu gostaria de receber.
                 </Text>
+            </View>
+            <View style={{ paddingTop: 20 }}>
+                {/* ProductsHeader */}
+                <View style={style.wishListHeaderContainer}>
+                    <Text style={style.wishListHeaderTitle}>
+                        Minha lista de desejos:
+                    </Text>
+                    <TouchableOpacity style={style.addProductButton}>
+                        <FontAwesome5 name="plus" size={15} color="white" />
+                        <Text style={style.addProductButtonText}>
+                            Adicionar produto
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+                {/* Wishlist */}
+                <View style={style.wishListContainer}>
+                    {/* ProductContainer */}
+                    <View style={style.productContainer}>
+                        <View>
+                            <View style={style.categoryContainer}>
+                                <Image
+                                    source={CategoryIcon}
+                                    style={style.categoryIcon}
+                                />
+                                <Text style={style.categoryText}>Livro</Text>
+                            </View>
+                            <Text style={style.productText}>
+                                Neurociência Para Leigos
+                            </Text>
+                            <View style={style.buttonsContainer}>
+                                <TouchableOpacity>
+                                    <FontAwesome
+                                        name="heart-o"
+                                        size={20}
+                                        color="white"
+                                    />
+                                </TouchableOpacity>
+                                <TouchableOpacity
+                                    style={style.messageContainer}
+                                >
+                                    <Feather
+                                        name="message-circle"
+                                        size={20}
+                                        color="white"
+                                    />
+                                    <Text style={style.messageCount}>10</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                        <TouchableOpacity style={style.productImageContainer}>
+                            <Image
+                                source={ProductImage}
+                                style={style.productImage}
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity style={style.optionsButton}>
+                            <Feather
+                                name="more-horizontal"
+                                size={20}
+                                color="#FF6B6B"
+                            />
+                        </TouchableOpacity>
+                    </View>
+                </View>
             </View>
         </View>
     );
--- Mobile/src/pages/profile/styles.js
+++ Mobile/src/pages/profile/styles.js
@@ -69,4 +69,125 @@ export const style = StyleSheet.create({
         color: 'white',
     },
     //#endregion
+
+    //#region WishListHeaderStyles
+    wishListHeaderContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
+
+    wishListHeaderTitle: {
+        color: '#595959',
+        fontFamily: 'Roboto-Light',
+        fontSize: 18,
+    },
+
+    addProductButton: {
+        width: '40%',
+        height: 30,
+        paddingHorizontal: 10,
+        backgroundColor: '#FF7A7A',
+        borderRadius: 50,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
+
+    addProductButtonText: {
+        color: 'white',
+        fontFamily: 'Roboto-Light',
+    },
+    //#endregion
+
+    //#region WishListStyles
+
+    wishListContainer: {
+        width: '92%',
+        marginTop: 20,
+        paddingVertical: 20,
+        backgroundColor: '#F1F1F1',
+        borderRadius: 9,
+        elevation: 4,
+        alignItems: 'center',
+        alignSelf: 'center',
+    },
+
+    productContainer: {
+        width: '90%',
+        height: 116,
+
+        paddingVertical: 10,
+        paddingLeft: 20,
+        borderRadius: 8,
+        backgroundColor: '#FF6B6B',
+
+        flexDirection: 'row',
+    },
+
+    categoryContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        // justifyContent: 'center',
+    },
+
+    categoryIcon: {
+        width: 18,
+        height: 18,
+        marginTop: 3,
+    },
+
+    categoryText: {
+        marginLeft: 5,
+        color: 'white',
+        fontSize: 16,
+        fontFamily: 'Roboto-Light',
+    },
+
+    productText: {
+        marginTop: 10,
+        color: 'white',
+        fontSize: 16,
+        fontFamily: 'Roboto-Light',
+    },
+
+    buttonsContainer: {
+        marginTop: 20,
+        flexDirection: 'row',
+    },
+
+    messageContainer: {
+        marginLeft: 20,
+        flexDirection: 'row',
+    },
+
+    messageCount: {
+        marginLeft: 5,
+        color: 'white',
+    },
+
+    productImageContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    productImage: {
+        width: '100%',
+        height: 103,
+        resizeMode: 'contain',
+    },
+
+    optionsButton: {
+        width: 20,
+        height: 20,
+        borderRadius: 50,
+        position: 'absolute',
+        right: 10,
+        top: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#F5F5F5',
+    },
+    //#endregion
 });
