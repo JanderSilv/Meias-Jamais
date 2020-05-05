4fffd45fbde2133ae0b206e6d1c50970a5d4ece4
Ruannilton <ruannilton@gmail.com>
2020-05-05 00:43:54 -0300

    rotas corrigidas

--- Server/src/controllers/userControler.js
+++ Server/src/controllers/userControler.js
@@ -2,91 +2,89 @@ const connection = require('./../database/connetcion');
 
 module.exports = {
 
-    index(request,response){
-        connection('usuario').select('*').then(res=>{
+    index(request, response) {
+        connection('usuario').select('*').then(res => {
             response.json(res);
-        }).catch(error=>{
-            response.json({err:error,msg: error.toString()});
+        }).catch(error => {
+            response.json({ err: error, msg: error.toString() });
         })
     },
 
-    get(request,response){
-        const {id} = request.params;
-        connection('usuario').where('id',id).then(res=>{
+    get(request, response) {
+        const { id } = request.params;
+        connection('usuario').where('id', id).then(res => {
             response.json(res);
-        }).catch(error=>{
-            response.json({err:error,msg: error.toString()});
+        }).catch(error => {
+            response.json({ err: error, msg: error.toString() });
         })
     },
 
-    create(request,response){
-        const{nome,nome_de_usuario,descricao,dt_aniversario} = request.body;
+    create(request, response) {
+        const { nome, nome_de_usuario, descricao, dt_aniversario } = request.body;
         connection('usuario').insert(
             {
-            nome,
-            nome_de_usuario,
-            descricao,
-            dt_criacao: Date.now().toString(),
-            dt_aniversario:new Date(dt_aniversario)
-            }).then(res=>{
+                nome,
+                nome_de_usuario,
+                descricao,
+                dt_criacao: Date.now().toString(),
+                dt_aniversario: new Date(dt_aniversario)
+            }).then(res => {
                 const [id] = res;
-                return response.json({id});
-            }).catch(error=>{
-                response.json({err:error,msg: error.toString()});
+                return response.json({ id });
+            }).catch(error => {
+                response.json({ err: error, msg: error.toString() });
             })
     },
 
-    delete(request,response){
-        const {id} = request.params
-        connection('usuario').where('id',id).del().then(res=>{
+    delete(request, response) {
+        const { id } = request.params
+        connection('usuario').where('id', id).del().then(res => {
             response.json(res);
-        }).catch(error=>{
-            response.json({err:error,msg: error.toString()});
+        }).catch(error => {
+            response.json({ err: error, msg: error.toString() });
         })
     },
 
-    addFolower(request,response){
-        const {id,id_seguido} = request.body
+    addFolower(request, response) {
+        const { id, id_seguido } = request.body
         connection('usuario_usuario').insert({
             usuario_id: id,
-            usuario_seguido_id:id_seguido
-        }).then(res=>{
+            usuario_seguido_id: id_seguido
+        }).then(res => {
             const [id] = res
-            return response.json({id})
-        }).catch(error=>{
-            response.json({err:error,msg: error.toString()});
+            return response.json({ id })
+        }).catch(error => {
+            response.json({ err: error, msg: error.toString() });
         })
     },
 
-    remFolower(request,response){
-        const {id,id_removido} = request.body
+    remFolower(request, response) {
+        const { id, id_removido } = request.body
         connection('usuario_usuario')
-        .where('usuario_id',id).where('usuario_seguido_id', id_removido)
-        .del()
-        .then(res=>{
-            return response.json(res)
-        }).catch(error=>{
-            response.json({err:error,msg: error.toString()});
+            .where('usuario_id', id).where('usuario_seguido_id', id_removido)
+            .del()
+            .then(res => {
+                return response.json(res)
+            }).catch(error => {
+                response.json({ err: error, msg: error.toString() });
+            })
+    },
+
+    getFollower(request, response) {
+        const { id } = request.params
+        connection('usuario_usuario').where('usuario_seguido_id', id).select('usuario_id').then(res => {
+            return response.json(res);
+        }).catch(error => {
+            response.json({ err: error, msg: error.toString() });
         })
     },
 
-    getFollower(request,response){
-        const {id} = request.params
-        console.log({id})
-        connection('usuario_usuario').where('usuario_seguido_id', id).then(res=>{
+    getFollowing(request, response) {
+        const { id } = request.params
+        connection('usuario_usuario').where('usuario_id', id).select('usuario_seguido_id').as('usuario_id').then(res => {
             return response.json(res);
-        }).catch(error =>{
-            response.json({err:error,msg: error.toString()});
-        })
-    },
-
-    getFollowing(request,response){
-        const {id} = request.params
-        console.log({id})
-        connection('usuario_usuario').where('usuario_id',id).then(res=>{
-            return response.json(res);
-        }).catch(error =>{
-            response.json({err:error,msg: error.toString()});
+        }).catch(error => {
+            response.json({ err: error, msg: error.toString() });
         })
     }
 }
--- Server/src/server/router.js
+++ Server/src/server/router.js
@@ -5,16 +5,32 @@ const routes = express.Router();
 const userControler = require('./../controllers/userControler')
 
 
-routes.get('/user',userControler.index)
-routes.get('/user/:id',userControler.get)
-routes.get('/user/getFollower',userControler.getFollower)
-routes.get('/user/getFollowing',userControler.getFollowing)
+routes.get('/user', userControler.index)        // retorna todos os usuarios
+routes.get('/user/:id', userControler.get)      // retorna um usuário específico
 
-routes.post('/user/create',userControler.create)
-routes.post('/user/addFollower',userControler.addFolower)
+routes.get('/user/getFollower/:id', userControler.getFollower)      // retorna os seguidores de um usuário
+routes.get('/user/getFollowing/:id', userControler.getFollowing)        // retorna os usuários que este usuário segue
 
-routes.delete('/user/remFollower',userControler.remFolower)
-routes.delete('/user/remove/:id',userControler.delete)
+routes.post('/user/create', userControler.create)       // cria um novo usuário
+/*
+{
+   "nome":"Ruan",
+   "nome_de_usuario":"rn",
+   "descricao":"nice guy",
+   "dt_aniversario":"2000-03-16"
+}
+*/
+
+routes.post('/user/addFollower', userControler.addFolower)          // passa a seguir um usuário
+/*
+{
+    "id":1,
+	"id_seguido":2
+}
+*/
+
+routes.delete('/user/remove/:id', userControler.delete)     //delete um usuário
+routes.delete('/user/remFollower', userControler.remFolower)        // para de seguir um usuário
 
 
 module.exports = routes;
