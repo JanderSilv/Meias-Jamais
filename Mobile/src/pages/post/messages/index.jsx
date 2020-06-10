33defed8694ad3221a2b56b3fef4b6375567fd8b
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2020-06-10 00:08:40 -0300

    Implementação visual da página de comentários

---
+++ Mobile/src/pages/post/messages/index.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import { View, Text, Image, TouchableOpacity, TextInput } from 'react-native';
+import { MaterialIcons, FontAwesome, Feather } from '@expo/vector-icons';
+
+import { style } from './styles';
+
+import AlexBracken from '../../../assets/main/alexBracken.png';
+import CategoryIcon from '../../../assets/main/categoryIcon.png';
+import ProductImage from '../../../assets/main/productImage.png';
+
+export default function messages() {
+    const [message, setMessage] = useState('');
+
+    return (
+        <View style={{ flex: 1 }}>
+            <View style={style.postContainer}>
+                <View style={style.postDescriptionContainer}>
+                    <Image source={AlexBracken} style={style.userImage} />
+                    <Text style={style.postDescription}>
+                        <Text
+                            style={{
+                                color: 'black',
+                            }}
+                        >
+                            Alex Bracken{' '}
+                        </Text>
+                        adicionou um novo item na lista de desejos
+                    </Text>
+                </View>
+                <View style={style.productContainer}>
+                    <View style={{ justifyContent: 'center' }}>
+                        <View style={style.categoryContainer}>
+                            <Image
+                                source={CategoryIcon}
+                                style={style.categoryIcon}
+                            />
+                            <Text style={style.categoryText}>Livro</Text>
+                        </View>
+                        <Text style={style.productText}>
+                            Neurociência Para Leigos
+                        </Text>
+                    </View>
+                    <TouchableOpacity style={style.productImageContainer}>
+                        <Image
+                            source={ProductImage}
+                            style={style.productImage}
+                        />
+                    </TouchableOpacity>
+                </View>
+            </View>
+            <View style={style.postTime}>
+                <Feather name="clock" size={16} color="#A5A5A5" />
+                <Text style={style.postTimeText}>2 dias atrás</Text>
+            </View>
+            <View style={style.likeContainer}>
+                <TouchableOpacity>
+                    <FontAwesome name="heart-o" size={18} color="#707070" />
+                </TouchableOpacity>
+                <Text style={style.likeText}>Curtir</Text>
+            </View>
+            <View>
+                <View style={style.messageContainer}>
+                    <TouchableOpacity>
+                        <Image source={AlexBracken} style={style.userImage} />
+                    </TouchableOpacity>
+                    <View style={style.messageDataContainer}>
+                        <TouchableOpacity>
+                            <Text style={style.messageUser}>@Jander.Silv</Text>
+                        </TouchableOpacity>
+                        <Text style={style.messageText}>
+                            Quero um desse também
+                        </Text>
+                        <Text style={style.messageTime}>Agora há pouco</Text>
+                    </View>
+                </View>
+            </View>
+            <View style={style.messageInputWrapper}>
+                <View style={style.messageInputContainer}>
+                    <TextInput
+                        style={style.messageInputText}
+                        placeholder={'Deixe um comentário...'}
+                        placeholderTextColor={'#929292'}
+                        value={message}
+                        onChangeText={setMessage}
+                    />
+                    <TouchableOpacity disabled={!message ? true : false}>
+                        <MaterialIcons
+                            name="send"
+                            size={25}
+                            color={message ? '#FF6B6B' : '#929292'}
+                        />
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    );
+}
---
+++ Mobile/src/pages/post/messages/styles.js
@@ -0,0 +1,159 @@
+import { StyleSheet } from 'react-native';
+
+export const style = StyleSheet.create({
+    postContainer: {
+        paddingTop: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    postDescriptionContainer: {
+        marginTop: 10,
+        paddingHorizontal: 50,
+        flexDirection: 'row',
+    },
+
+    userImage: {
+        width: 54,
+        height: 54,
+        borderRadius: 50,
+    },
+
+    postDescription: {
+        marginLeft: 10,
+        fontFamily: 'Roboto-Light',
+        fontSize: 16,
+        color: 'gray',
+    },
+
+    productContainer: {
+        width: '90%',
+        height: 116,
+
+        marginTop: 10,
+        paddingVertical: 10,
+        paddingLeft: 20,
+        borderRadius: 8,
+        backgroundColor: '#FF6B6B',
+
+        flexDirection: 'row',
+    },
+
+    categoryContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        // justifyContent: 'center',
+    },
+
+    categoryIcon: {
+        width: 18,
+        height: 18,
+        marginTop: 3,
+    },
+
+    categoryText: {
+        marginLeft: 5,
+        color: 'white',
+        fontSize: 16,
+        fontFamily: 'Roboto-Light',
+    },
+
+    productText: {
+        marginTop: 10,
+        color: 'white',
+        fontSize: 16,
+        fontFamily: 'Roboto-Light',
+    },
+
+    productImageContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    productImage: {
+        width: '100%',
+        height: 103,
+        resizeMode: 'contain',
+    },
+
+    postTime: {
+        marginTop: 10,
+        paddingLeft: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+
+    postTimeText: {
+        marginLeft: 10,
+        fontFamily: 'Roboto-Light',
+        color: '#A5A5A5',
+    },
+
+    likeContainer: {
+        width: '100%',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        marginTop: 20,
+        borderWidth: 0.5,
+        borderColor: '#D4D4D4',
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+
+    likeText: {
+        marginLeft: 10,
+        fontFamily: 'Roboto-Light',
+    },
+
+    messageContainer: {
+        marginTop: 20,
+        paddingLeft: 20,
+        flexDirection: 'row',
+    },
+
+    messageDataContainer: {
+        minHeight: 70,
+        marginLeft: 10,
+        justifyContent: 'space-around',
+    },
+
+    messageUser: {
+        fontFamily: 'Roboto-Light',
+        fontSize: 16,
+        color: 'black',
+    },
+
+    messageText: {
+        fontFamily: 'Roboto-Light',
+        color: '#5F5F5F',
+    },
+
+    messageTime: {
+        fontFamily: 'Roboto-Light',
+        color: '#989898',
+        fontSize: 11,
+    },
+
+    messageInputWrapper: {
+        paddingBottom: 10,
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+
+    messageInputContainer: {
+        backgroundColor: '#F5F5F5',
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+
+    messageInputText: {
+        width: '80%',
+        marginRight: 20,
+        borderBottomWidth: 1,
+        borderBottomColor: '#FF6B6B',
+        fontSize: 18,
+        color: '#212121',
+    },
+});
