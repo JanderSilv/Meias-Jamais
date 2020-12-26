7f8f2b598b0e239ebb56e06331bce08cfc0c29a4
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2020-12-26 00:25:55 -0300

    feat: Implementação do Index de Services

---
+++ Mobile/src/services/index.ts
@@ -0,0 +1,9 @@
+import api from './api';
+
+import AuthApi from './useCases/authentication';
+import { TypeUser } from './models/authentication';
+
+export { api };
+/* Authentication */
+export { AuthApi };
+export type { TypeUser };
